ba37bfba5f568367816a6e1243c1ac204e1e78f3
Tyron Burkhardt <tyron.burkhardt.za@gmail.com>
2026-07-06 13:14:11 +0200

    Remove summary comments from Main method
    Removed XML summary comments from the Main method.

--- DayZ Manage Crash Logs/Program.cs
+++ DayZ Manage Crash Logs/Program.cs
@@ -8,9 +8,6 @@ namespace DayZ_Manage_Crash_Logs
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
